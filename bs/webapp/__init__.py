017ac2bc512a33e49589760f2c73cdcafbda9067
PiTiLeZarD <pitilezard@gmail.com>
2011-03-04 09:08:03 +0100

    layout ++, episode has its own page

--- bs/webapp/__init__.py
+++ bs/webapp/__init__.py
@@ -22,12 +22,17 @@ def serie(id):
     serie = model.Session.query(model.Serie).get(id)
     return render_template('serie.html', serie=serie)
 
-@app.route('/episodes/<serie_id>/<season_num>')
-def episodes(serie_id, season_num):
-    serie = model.Session.query(model.Serie).get(serie_id)
+@app.route('/episodes/<id_serie>/<season_num>')
+def episodes(id_serie, season_num):
+    serie = model.Session.query(model.Serie).get(id_serie)
     seasons = filter(lambda item: item.num == season_num, serie.seasons)
     assert(len(seasons) == 1)
     return render_template('episodes.html', serie=serie, season=seasons[0])
+    
+@app.route('/episode/<id_serie>/<episode_num>')
+def episode(id_serie, episode_num):
+	episode = model.Session.query( model.Episode ).filter_by(num=episode_num).join( model.Season ).filter_by(id_serie=id_serie).one()
+	return render_template('episode.html', episode=episode)
 
 @app.route('/downloads')
 def downloads():
